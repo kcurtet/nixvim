7e26d85e0058b073d27c8834d1ca90226500c318
Kevin Curtet <kcurtet@gmail.com>
2026-01-10 02:39:15 +0100

    1768009155-switch

--- config/nixos-path.nix
+++ config/nixos-path.nix
@@ -10,11 +10,41 @@
       default = "/home/kx/.config/nixos";
       description = "Path to NixOS configuration for nixd LSP";
     };
-    
+
     nixvimConfigPath = lib.mkOption {
       type = lib.types.str;
       default = "/home/kx/Code/nixvim";
       description = "Path to NixVim configuration";
     };
   };
+
+  config = {
+    extraConfigLua = ''
+      local nix_files_modified = false
+
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = {
+            "/etc/nixos/*.nix",
+            "/etc/nixos/**/*.nix",
+            "${config.nixvimConfigPath}/**/*.nix",
+            "${config.nixosConfigPath}/**/*.nix",
+          },
+          callback = function()
+            nix_files_modified = true
+          end,
+        })
+
+        vim.api.nvim_create_autocmd("VimLeavePre", {
+          callback = function()
+            if nix_files_modified then
+              print("Running nixos-rebuild switch...")
+              vim.fn.system("pushd ${config.nixvimConfigPath}; git commit -am $(date +%s)-switch; git push; popd")
+              vim.fn.system("pushd ${config.nixvimConfigPath}; git commit -am $(date +%s)-switch; git push; popd")
+              vim.fn.system("sudo nixos-rebuild switch")
+            end
+          end,
+        })
+    '';
+  };
 }
+
