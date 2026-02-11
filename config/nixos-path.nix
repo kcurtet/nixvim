fbf73ee6cbe9653b6de8afd29784d4a96d0853c3
Kevin Curtet <kcurtet@gmail.com>
2026-02-11 20:17:06 +0100

    1770837426-switch

--- config/nixos-path.nix
+++ config/nixos-path.nix
@@ -17,39 +17,4 @@
       description = "Path to NixVim configuration";
     };
   };
-
-  config = {
-    autoCmd = [
-      {
-        event = ["BufWritePost"];
-        pattern = [
-          "${config.nixvimConfigPath}/*.nix"
-          "${config.nixvimConfigPath}/**/*.nix"
-        ];
-        callback = {
-          __raw = ''
-            function()
-                vim.g.nix_files_modified = true
-                print("Running git commit on ${config.nixvimConfigPath}...")
-                vim.fn.system("pushd ${config.nixvimConfigPath}; git commit -am $(date +%s)-switch; git push; popd")
-            end
-          '';
-        };
-      }
-      {
-        event = ["VimLeavePre"];
-        callback = {
-          __raw = ''
-            function()
-              if vim.g.nix_files_modified then
-                print("Running nixos-rebuild switch...")
-                vim.fn.system("pushd ${config.nixosConfigPath}; nix flake update nixvim; popd")
-                vim.fn.system("nswitch")
-              end
-            end
-          '';
-        };
-      }
-    ];
-  };
 }
