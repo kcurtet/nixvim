ff2ae59bc6cde72931d250967d5e467e5e179f49
Kevin Curtet <kcurtet@gmail.com>
2026-02-12 22:00:39 +0100

    1770930039-switch

--- config/system.nix
+++ config/system.nix
@@ -3,6 +3,7 @@
   vimAlias = true;
   enableMan = false;
   enablePrintInit = true; # nixvim-print-init tool
+  nixpkgs.config.allowUnfree = true;
 
   clipboard.providers.wl-copy.enable = true;
   clipboard.providers.xclip.enable = false;
