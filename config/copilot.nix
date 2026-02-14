2ba72c2f7f9c8a008c6b1c658acea9a052707a07
Kevin Curtet <kcurtet@gmail.com>
2026-02-14 16:40:07 +0100

    Change copilot.vim to copilot.lua

--- config/copilot.nix
+++ config/copilot.nix
@@ -1,5 +1,36 @@
 {
   plugins = {
-    copilot-vim.enable = true;
+    copilot-lua = {
+      enable = true;
+      luaConfig.content = ''
+        require('copilot').setup({
+          panel = {
+            enabled = true,
+            auto_refresh = true,
+            keymap = {
+              jump_prev = '[[',
+              jump_next = ']]',
+              accept = '<CR>',
+              refresh = 'gr',
+              open = '<M-CR>'
+            },
+          },
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            debounce = 75,
+            keymap = {
+              accept = '<M-l>',
+              accept_word = false,
+              accept_line = false,
+              next = '<M-]>',
+
+              prev = '<M-[>',
+              dismiss = '<C-]>',
+            },
+          },
+        })
+      '';
+    };
   };
 }
