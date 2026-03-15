47779e57723a945ce97897562c4eb81ba08acc84
Kevin Curtet <kcurtet@gmail.com>
2026-03-15 21:26:43 +0100

    fix(harpoon): use correct config format to resolve tbl_extend error
    The enableTelescope option caused harpoon to receive settings in an
    incorrect format, leading to a tbl_extend error. Changed to use
    luaConfig.content with the proper settings wrapper.

--- config/navigation.nix
+++ config/navigation.nix
@@ -1,18 +1,24 @@
 {
   plugins = {
     neo-tree.enable = true;
-    telescope.enable = true;
+    telescope = {
+      enable = true;
+      enabledExtensions = [ "harpoon" ];
+    };
     flash.enable = true;
     project-nvim.enable = true;
 
     # Harpoon2 - Quick file navigation
     harpoon = {
       enable = true;
-      enableTelescope = true;
-      settings = {
-        save_on_toggle = true;
-        sync_on_ui_close = false;
-      };
+      luaConfig.content = ''
+        require("harpoon"):setup({
+          settings = {
+            save_on_toggle = true,
+            sync_on_ui_close = false,
+          }
+        })
+      '';
     };
   };
 }
